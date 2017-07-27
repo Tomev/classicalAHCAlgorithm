70d3b09574eb74332e2228119aa4b52e9d1bac5d
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2017-07-27 23:32:12 +0200

    [STABLE] Custom object similarity measure done.

--- objectSimilarityMeasures/customObjectSimilarityMeasure.cpp
+++ objectSimilarityMeasures/customObjectSimilarityMeasure.cpp
@@ -1,16 +1,49 @@
 #include "customObjectSimilarityMeasure.h"
 
-customObjectSimilarityMeasure::customObjectSimilarityMeasure(vector<attributeData> *attributesData) :
-  attributesData(attributesData)
+#include <algorithm>
+
+customObjectSimilarityMeasure::customObjectSimilarityMeasure(unordered_map<string, attributeData*> *attributesData,
+                                                             attributesSimilarityMeasure *nAttrSimMeasure,
+                                                             attributesSimilarityMeasure *cAttrSimMeasure) :
+  attributesData(attributesData), nAttributeSimilarityMeasure(nAttrSimMeasure),
+  cAttributeSimilarityMeasure(cAttrSimMeasure)
 {}
 
 double customObjectSimilarityMeasure::countObjectsSimilarity(sample *o1, sample *o2)
 {
   double result = 0;
 
+  findCommonAttributes(o1, o2);
 
+  for(string attributeName : commonAttributes)
+  {
+    if(attributesData->at(attributeName)->getType() == "numerical")
+      result += nAttributeSimilarityMeasure->countAttributesSimilarity(o1->attributesValues.at(attributeName),
+                                                                       o2->attributesValues.at(attributeName));
+    else
+      result += cAttributeSimilarityMeasure->countAttributesSimilarity( o1->attributesValues.at(attributeName),
+                                                                        o2->attributesValues.at(attributeName));
+  }
 
-  return result;
+  return result / commonAttributes.size();
 }
 
+void customObjectSimilarityMeasure::findCommonAttributes(sample *sample1, sample *sample2)
+{
+  commonAttributes.clear();
+
+  vector<string> firstSampleAttributes, secondSampleAttributes;
+
+  for(auto nameValue : sample1->attributesValues) firstSampleAttributes.push_back(nameValue.first);
+  for(auto nameValue : sample2->attributesValues) secondSampleAttributes.push_back(nameValue.first);
+
+  for(string attributeName : firstSampleAttributes)
+  {
+    if(find(secondSampleAttributes.begin(), secondSampleAttributes.end(), attributeName) != firstSampleAttributes.end())
+    {
+      // If both contains given attribute
+      commonAttributes.push_back(attributeName);
+    }
+  }
+}
 
--- objectSimilarityMeasures/customObjectSimilarityMeasure.h
+++ objectSimilarityMeasures/customObjectSimilarityMeasure.h
@@ -4,16 +4,23 @@
 #include <vector>
 #include "objectsSimilarityMeasure.h"
 #include "../attributesData/attributeData.h"
+#include "attributesSimilarityMeasures/attributesSimilarityMeasure.h"
 
 class customObjectSimilarityMeasure : public objectsSimilarityMeasure
 {
   public:
-    customObjectSimilarityMeasure(vector<attributeData> *attributesData);
+    customObjectSimilarityMeasure(unordered_map<string, attributeData*> *attributesData, attributesSimilarityMeasure* nAttrSimMeasure,
+                                  attributesSimilarityMeasure* cAttrSimMeasure);
     double countObjectsSimilarity(sample* o1, sample* o2);
 
   protected:
-    vector<attributeData> *attributesData;
+    unordered_map<string, attributeData*> *attributesData;
+    attributesSimilarityMeasure* nAttributeSimilarityMeasure;
+    attributesSimilarityMeasure* cAttributeSimilarityMeasure;
 
+    vector<string> commonAttributes;
+
+    void findCommonAttributes(sample *sample1, sample *sample2);
 };
 
 #endif //CLASSICALAHCALGORITHM_CUSTOMOBJECTSIMILARITYMEASURE_H
